12f311ac8293ba121b30f8f6a049ef54c17d379d
AciDCooL <arno.kattenpoel@gmail.com>
2018-10-27 01:18:49 +0200

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -4,8 +4,11 @@ import requests
 import json
 import sys
 
+domoticzip = '1'
 myusername = '1'
 mypassword = '1'
+idxwatt2 = '24'
+idxwatt1 = '23'
 idxwatt = '22'
 idxkwh = '21'
 solaxsite = 'www.solaxcloud.com:6080'
@@ -23,9 +26,17 @@ try:
     alldataurl = 'http://'+solaxsite+'/proxy//mysite/getInverterInfo?siteId='+str(mysitedata['result'][0]['siteId'])+'&tokenId='+tokendata['result']['tokenId']
     try:
       alldata = requests.post(alldataurl).json()
-      ##Watt
+      ##Watt Totaal
       current = alldata['result'][0]['pv1Current']+alldata['result'][0]['pv2Current']
-      domurl = 'http://192.168.0.157:80/json.htm?type=command&param=udevice&nvalue=0&idx='+idxwatt+'&svalue='+str(current)
+      domurl = 'http://'+domoticzip+':80/json.htm?type=command&param=udevice&nvalue=0&idx='+idxwatt+'&svalue='+str(current)
+      requests.get(domurl)
+      ##Watt Paneelgroep1
+      current1 = alldata['result'][0]['pv1Current']
+      domurl = 'http://'+domoticzip+':80/json.htm?type=command&param=udevice&nvalue=0&idx='+idxwatt+'&svalue='+str(current1)
+      requests.get(domurl)
+      ##Watt Paneelgroep2
+      current2 = alldata['result'][0]['pv2Current']
+      domurl = 'http://'+domoticzip+':80/json.htm?type=command&param=udevice&nvalue=0&idx='+idxwatt+'&svalue='+str(current2)
       requests.get(domurl)
     except requests.exceptions.RequestException as e:
       print(e)

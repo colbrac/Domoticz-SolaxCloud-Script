2091173aacbdec6d89516016c80794297102b134
AciDCooL <arno.kattenpoel@gmail.com>
2018-10-27 00:59:24 +0200

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -1,33 +1,38 @@
-import requests
+#!/usr/bin/python3
 
-myusername = '123'
-mypassword = '123'
+import requests
+import json
+import sys
+
+myusername = '1'
+mypassword = '1'
+idxwatt = '22'
+idxkwh = '21'
 solaxsite = 'www.solaxcloud.com:6080'
 tokenurl = 'http://'+solaxsite+'/proxy//login/login?password='+mypassword+'&userName='+myusername+'&userType=5'
 mysiteurl = 'http://'+solaxsite+'/proxy//mysite/mySite'
 
-tokendata = requests.post(tokenurl).json()
-if tokendata['success']:
-  #print("It Works.. My tokenID is "+tokendata['result']['tokenId'])
-  #print("It Works.. My userID is "+tokendata['result']['userId'])
+try:
+  tokendata = requests.post(tokenurl).json()
   tokenanduser= {
     'tokenId': tokendata['result']['tokenId'],
     'userId': tokendata['result']['userId'],
     }
-  mysitedata = requests.post(mysiteurl, data=tokenanduser).json()
-  if mysitedata['success']:
-    #deze result bestaat uit een array (zie json resultaat)
-    #print("It Works... My siteID is "+str(mysitedata['result'][0]['siteId']))
+  try:
+    mysitedata = requests.post(mysiteurl, data=tokenanduser).json()
     alldataurl = 'http://'+solaxsite+'/proxy//mysite/getInverterInfo?siteId='+str(mysitedata['result'][0]['siteId'])+'&tokenId='+tokendata['result']['tokenId']
-    alldata = requests.post(alldataurl).json()
-    if alldata['success']:
-      print("It Works... now eat my dataaaaaaaa")
-      print(alldata)
-    else:
-     print("Invalid siteid")
-  else:
-    print("Invalid token or user")
-else:
-  print("Invalid credentials")
-
-
+    try:
+      alldata = requests.post(alldataurl).json()
+      ##Watt
+      current = alldata['result'][0]['pv1Current']+alldata['result'][0]['pv2Current']
+      domurl = 'http://192.168.0.157:80/json.htm?type=command&param=udevice&nvalue=0&idx='+idxwatt+'&svalue='+str(current)
+      requests.get(domurl)
+    except requests.exceptions.RequestException as e:
+      print(e)
+      sys.exit(1)
+  except requests.exceptions.RequestException as e:
+    print(e)
+    sys.exit(1)
+except requests.exceptions.RequestException as e:
+  print(e)
+  sys.exit(1)
